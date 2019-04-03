66a5ede61c5e5197ba9dd0c59015f21cb016a551
Adu Williams <aduwillie@gmail.com>
2019-04-03 15:35:49 +0000

    updated travis and setup_db script

--- src/tests/knexfile.js
+++ src/tests/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     connection: {
         database: process.env.DB_NAME,
         user: process.env.MYSQL_USER,
-        password: process.env.DB_PASSWORD,
+        password: process.env.NODE_ENV === "ci" ? "" : process.env.DB_PASSWORD,
         host: process.env.MYSQL_HOST
     },
     pool: {
